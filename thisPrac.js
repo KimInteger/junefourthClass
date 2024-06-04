ccdf3bf2c11b040027a13e5610af2fb0c41deca1
KimInteger <kjs20151240@gmail.com>
2024-06-04 09:36:04 +0900

    second함수 작성완료

--- thisPrac.js
+++ thisPrac.js
@@ -11,5 +11,28 @@ const THIS_IS_GLOBAL = global; // 전역 객체 그대로 할당
 
 // 화살표 함수
 const second = (a,b) => {
-  
+  let local = 1;
+  this.value = 200;
+  // 변수로 전역스코프를 가져온것과 같은 결과
+  console.log(THIS_IS_GLOBAL);
+  console.log('화살표 함수의 this가 가리키는 객체 : ', this);
+
+  // 내부 함수 선언 ------------------------------------------------
+  const inner = () => {
+    // 화살표 함수의 this는 상위 스코프의 this를 가리킴
+    console.log('내부 화살표 함수의 this가 가리키는 객체 : ', this);
+    // 상위 스코프의 this를 가리킴
+    console.log('this로 접근한 전역객체 키 : ', this.value);
+    console.log("전역객체 키 : ", THIS_IS_GLOBAL.value);
+    // 상위 스코프의 this를 가져온 사례 : 올바른 코드
+
+    // 아래는 second()함수의 지역변수 local을 가져온 것
+    // inner()함수의 입장에서는 지역변수가 아닌, 상위 스코프의 변수처럼 보임
+    console.log(local);
+
+    return this.value;
+  }
+
+  // 내부 함수에서 리턴된 값에 a와 b를 더한 값을 리턴
+  return inner() + a + b;
 }
